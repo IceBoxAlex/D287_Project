022ce98d790106236c8cfa3c2c78fb75879e265c
IceBoxAlex <114825763+IceBoxAlex@users.noreply.github.com>
2025-03-11 18:26:32 -0400

    Added logic for validating desired product inventory to part minimum inventory

--- src/main/java/com/example/demo/validators/PriceProductValidator.java
+++ src/main/java/com/example/demo/validators/PriceProductValidator.java
@@ -1,17 +1,15 @@
 package com.example.demo.validators;
 
-import com.example.demo.domain.Part;
-import com.example.demo.domain.Product;
-import com.example.demo.repositories.InhousePartRepository;
-import com.example.demo.repositories.ProductRepository;
-import com.example.demo.service.InhousePartServiceImpl;
-import com.example.demo.service.ProductService;
-import com.example.demo.service.ProductServiceImpl;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 
-import javax.validation.ConstraintValidator;
-import javax.validation.ConstraintValidatorContext;
+import com.example.demo.domain.Part;
+import com.example.demo.domain.Product;
+import com.example.demo.service.ProductService;
+import com.example.demo.service.ProductServiceImpl;
 
 /**
  *
@@ -38,6 +36,14 @@ public class PriceProductValidator implements ConstraintValidator<ValidProductPr
         double sumPartsPrice = 0;
         if (product.getId() != 0) {
             Product myProduct = repo.findById((int) product.getId());
+            double invDiff = product.getInv() - myProduct.getInv();
+            for (Part p: myProduct.getParts()) {
+                if(p.getInv() - invDiff < p.getMinInv()) {
+                constraintValidatorContext.disableDefaultConstraintViolation();
+                constraintValidatorContext.buildConstraintViolationWithTemplate("The inventory of " + p.getName() + "would fall below its minimum of " + p.getMinInv()).addConstraintViolation();
+                return false;
+            }
+            }
             for (Part p : myProduct.getParts()) sumPartsPrice = sumPartsPrice + p.getPrice();
             if (product.getPrice() >= sumPartsPrice) {
                 return true;
